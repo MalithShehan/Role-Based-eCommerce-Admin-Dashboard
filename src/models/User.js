fb7d559b3ed1cfed756c6ea315993f296b8f78e0
ASUS <shehansandakalum2003@gmail.com>
2026-04-11 22:43:01 +0530

    create user model

---
+++ src/models/User.js
@@ -0,0 +1,70 @@
+const { DataTypes } = require("sequelize");
+const bcrypt = require("bcryptjs");
+
+module.exports = (sequelize) => {
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primeryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: "Name is required" },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: { msg: "Must be a valid email address" },
+          notEmpty: { msg: "Email is required" },
+        },
+      },
+      password: {
+        typeof: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: "Password is required" },
+          len: {
+            args: [6, 200],
+            msg: "Password must be at least 6 characters long",
+          },
+        },
+      },
+      role: {
+        type: DataTypes.ENUM("admin", "user"),
+        allowNull: false,
+        defaultValue: "user",
+      },
+    },
+    {
+      tableName: "users",
+      timestamps: true,
+      hooks: {
+        beforeCreate: async (user) => {
+          if (user.password) {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(user.password, salt);
+          }
+        },
+        beforeUpdate: async (user) => {
+          if (user.changed("password")) {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(user.password, salt);
+          }
+        },
+      },
+    },
+  );
+
+  User.prototype.comparePassword = async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+  };
+  return User;
+};
