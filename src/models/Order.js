e87dce5407320801cb4cb3ef2005fe096cabb82b
ASUS <shehansandakalum2003@gmail.com>
2026-04-11 22:52:13 +0530

    create order model

---
+++ src/models/Order.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const Order = sequelize.define('Order', {
+        id: {
+            type: DataTypes.INTEGER,
+            primeryKey: true,
+            autoIncrement: true,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'users',
+                key: 'id',
+            },
+        },
+        totalAmount: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+            validate: {
+                min: {
+                    args: [0],
+                    msg: "Total amount must be a positive number",
+                },
+            },
+        },
+        status: {
+            type: DataTypes.ENUM('pending', 'processing', 'shipped', 'deliverd', 'cancelled'),
+            allowNull: false,
+            defaultValue: 'pending',
+        },
+    }, {
+        tableName: 'orders',
+        timestamps: true,
+    });
+    return Order;
+};
