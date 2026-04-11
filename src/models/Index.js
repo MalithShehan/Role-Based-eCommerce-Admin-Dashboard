79d4e31e763754f7f0d118da53e499108e220965
ASUS <shehansandakalum2003@gmail.com>
2026-04-11 23:06:36 +0530

    create model index (loader+associations)

---
+++ src/models/Index.js
@@ -0,0 +1,42 @@
+const { Sequelize} = require('sequelize');
+const dbConfig = require('../config/database');
+
+const sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,
+    {
+        host: dbConfig.host,
+        port: dbConfig.port,
+        dialect: dbConfig.dialect,
+        logging: dbConfig.logging,
+        pool: dbConfig.pool,
+        define: dbConfig.define,
+    }
+)
+
+const User = require('./User')(sequelize);
+const Category = require('./Category')(sequelize);
+const Product = require('./Product')(sequelize);
+const Order = require('./Order')(sequelize);
+const OrderItem = require('./OrderItem')(sequelize);
+const Setting = require('./Setting')(sequelize);
+
+User.hasMany(Order, { foreignKey: 'userId', as: 'orders' });
+Order.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+Order.hasMany(OrderItem, { foreignKey: 'orderId', as: 'orderItems' });
+OrderItem.belongsTo(Order, { foreignKey: 'orderId', as: 'order' });
+
+Product.hasMany(OrderItem, { foreignKey: 'productId', as: 'orderItems' });
+OrderItem.belongsTo(Product, { foreignKey: 'productId', as: 'product' });
+
+module.exports = {
+    sequelize,
+    User,
+    Category,
+    Product,
+    Order,
+    OrderItem,
+    Setting,
+};
