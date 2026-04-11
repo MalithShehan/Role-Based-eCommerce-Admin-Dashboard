f49591361e083977b98722d792c840f3aa8b5e7e
ASUS <shehansandakalum2003@gmail.com>
2026-04-11 22:56:38 +0530

    create order item model

---
+++ src/models/OrderItem.js
@@ -0,0 +1,51 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const OrderItem = sequelize.define('OrderItem', {
+        id: {
+            type: DataTypes.INTEGER,
+            primeryKey: true,
+            autoIncrement: true,
+        },
+        orderId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'orders',
+                key: 'id',
+            },
+        },
+        productId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'products',
+                key: 'id',
+            },
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: {
+                    args: [1],
+                    msg: "Quantity must be at least 1",
+                },
+            },
+        },
+        price: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+            validate: {
+                min: {
+                    args: [0],
+                    msg: "Price must be a positive number",
+                },
+            },
+        },
+    }, {
+        tableName: 'order_items',
+        timestamps: true,
+    });
+    return OrderItem;
+}
